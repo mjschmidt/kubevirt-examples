7787760168f5bde30466c9e44c7b3c6379497bf4
Nicholas Dorr <dorr.a.nicholas@gmail.com>
2019-12-31 15:46:43 +0000

    reorg the folder

--- no-storage/docker/build.sh
+++
@@ -1,3 +0,0 @@
-mv ~/CentOS-7-x86_64-GenericCloud-1907.qcow2   /home/mjschm3/centos7/docker/
-docker build -t mjschmidt/centos:7-x86_64-genericcloud-1907-2 .
-mv /home/mjschm3/centos7/docker/CentOS-7-x86_64-GenericCloud-1907.qcow2 ~/
